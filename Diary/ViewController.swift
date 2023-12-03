f2c3571f5783172d1af57e1e6e7f2ca8c3c225c5
silli-3072 <hrtmnr.3072@gmail.com>
2023-12-03 19:35:53 +0900

    Initial Commit

---
+++ Diary/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Diary
+//
+//  Created by 春田実利 on 2023/12/03.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
