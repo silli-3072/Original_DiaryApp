7420af313fd5f9f012364a656c52ca73b734931e
silli-3072 <hrtmnr.3072@gmail.com>
2023-12-04 10:50:17 +0900

    update: UIパーツの追加・SwiftFileの設定

---
+++ Diary/ViewDiaryViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ViewDiaryViewController.swift
+//  Diary
+//
+//  Created by 春田実利 on 2023/12/04.
+//
+
+import UIKit
+
+class ViewDiaryViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
