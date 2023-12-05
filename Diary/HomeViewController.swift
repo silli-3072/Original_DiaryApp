b4a5ef56884667c3b41b07753a20fc33cb61017c
silli-3072 <hrtmnr.3072@gmail.com>
2023-12-05 22:14:12 +0900

    update: ボタンの装飾

--- Diary/HomeViewController.swift
+++ Diary/HomeViewController.swift
@@ -6,9 +6,14 @@ class HomeViewController: UIViewController {
     var timeOfDay: String = ""
     
     @IBOutlet var dayLabel: UILabel!
+    @IBOutlet var addMorningButton: UIButton!
+    @IBOutlet var addNightButton: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        addMorningButton.layer.cornerRadius = 30
+        addNightButton.layer.cornerRadius = 30
         
     }
     
--- Diary/ViewDiaryViewController.swift
+++ Diary/ViewDiaryViewController.swift
@@ -2,9 +2,15 @@
 import UIKit
 
 class ViewDiaryViewController: UIViewController {
+    
+    @IBOutlet var morningButton: UIButton!
+    @IBOutlet var nightButton: UIButton!
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        morningButton.layer.cornerRadius = 20
+        nightButton.layer.cornerRadius = 20
         
     }
 
