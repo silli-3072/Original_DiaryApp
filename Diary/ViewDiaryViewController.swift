a0a35a95bc7b9dc54b01d60b16a9aba13cf2d415
silli-3072 <hrtmnr.3072@gmail.com>
2023-12-08 11:51:24 +0900

    update: カレンダーの導入

--- Diary/ViewDiaryViewController.swift
+++ Diary/ViewDiaryViewController.swift
@@ -1,17 +1,72 @@
 
 import UIKit
+import FSCalendar
+import CalculateCalendarLogic
 
-class ViewDiaryViewController: UIViewController {
+class ViewDiaryViewController: UIViewController, FSCalendarDelegate, FSCalendarDataSource, FSCalendarDelegateAppearance {
     
     @IBOutlet var morningButton: UIButton!
     @IBOutlet var nightButton: UIButton!
-
+    @IBOutlet var calendar: FSCalendar!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        self.calendar.dataSource = self
+        self.calendar.delegate = self
         
         morningButton.layer.cornerRadius = 20
         nightButton.layer.cornerRadius = 20
         
     }
 
+    fileprivate let gregorian: Calendar = Calendar(identifier: .gregorian)
+    
+    fileprivate lazy var dateFormatter: DateFormatter = {
+        let formatter = DateFormatter()
+        formatter.dateFormat = "yyyy-MM-dd"
+        return formatter
+    }()
+
+    func judgeHoliday(_ date : Date) -> Bool {
+        let tmpCalendar = Calendar(identifier: .gregorian)
+
+        let year = tmpCalendar.component(.year, from: date)
+        let month = tmpCalendar.component(.month, from: date)
+        let day = tmpCalendar.component(.day, from: date)
+
+        let holiday = CalculateCalendarLogic()
+
+        return holiday.judgeJapaneseHoliday(year: year, month: month, day: day)
+    }
+    
+    func getDay(_ date:Date) -> (Int,Int,Int){
+        let tmpCalendar = Calendar(identifier: .gregorian)
+        let year = tmpCalendar.component(.year, from: date)
+        let month = tmpCalendar.component(.month, from: date)
+        let day = tmpCalendar.component(.day, from: date)
+        return (year,month,day)
+    }
+
+    func getWeekIdx(_ date: Date) -> Int{
+        let tmpCalendar = Calendar(identifier: .gregorian)
+        return tmpCalendar.component(.weekday, from: date)
+    }
+
+    func calendar(_ calendar: FSCalendar, appearance: FSCalendarAppearance, titleDefaultColorFor date: Date) -> UIColor? {
+        if self.judgeHoliday(date){
+            return UIColor.red
+        }
+
+        let weekday = self.getWeekIdx(date)
+        
+        if weekday == 1 {
+            return UIColor.red
+        } else if weekday == 7 {
+            return UIColor.blue
+        }
+
+        return nil
+    }
+    
 }
