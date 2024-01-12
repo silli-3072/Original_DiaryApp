a09523fd6218b6ac42e93df96e73c1063a78ab2c
silli-3072 <hrtmnr.3072@gmail.com>
2024-01-12 14:38:24 +0900

    update: カスタムセルの設定

--- Diary/SettingViewController.swift
+++ Diary/SettingViewController.swift
@@ -1,11 +1,31 @@
 
 import UIKit
 
-class SettingViewController: UIViewController {
-
+class SettingViewController: UIViewController, UITableViewDataSource {
+    
+    @IBOutlet var notificationTableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+        notificationTableView.register(UINib(nibName: "PermissionTableViewCell", bundle: nil), forCellReuseIdentifier:  "PermissionCell")
+        notificationTableView.register(UINib(nibName: "TimeScheduleTableViewCell", bundle: nil), forCellReuseIdentifier:  "TimeScheduleCell")
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 2
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let permissionCell = notificationTableView.dequeueReusableCell(withIdentifier: "PermissionCell") as! PermissionTableViewCell
+        let timeScheduleCell = notificationTableView.dequeueReusableCell(withIdentifier: "TimeScheduleCell") as! TimeScheduleTableViewCell
+        
+        switch indexPath.row {
+        case 0: return permissionCell
+        case 1: return timeScheduleCell
+        default: return timeScheduleCell
+        }
+        
     }
     
 }
